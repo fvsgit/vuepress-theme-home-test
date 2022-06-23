ae9d9e9c039ec7e9474e97840e3b225920eb4886
fvsgit <19401449+fvsgit@users.noreply.github.com>
2022-06-23 21:15:56 +0200

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,8 +5,7 @@ export default defineUserConfig({
   lang: "en-US",
   title: "Theme Demo",
   description: "A demo for vuepress-theme-hope",
-
+  hostname: "https://fvsgit.github.io",
   base: "/vuepress-theme-home-test/",
-
   theme,
 });
